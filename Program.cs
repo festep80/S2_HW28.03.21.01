3d23e79b8098acba6dcfa92244a1f1b955efa84d
festep80 <74856291+festep80@users.noreply.github.com>
2021-03-20 17:25:29 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//1. Створити програму, яка вводить з клавіатури символи(посимвольно- Console.Read())
+// та перетворює малі букви у великі та навпаки.
+
+using System;
+
+namespace S2_HW28._03._21._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter letter (0 - exit):");
+            char v = '1';
+            while(v!='0')
+            {                
+                v = (char)Console.Read();
+                LetterCaseConvert(v);                
+            }          
+        }
+
+        private static void LetterCaseConvert(char v)
+        {
+            if(Char.IsLower(v) == true)
+            {
+                Console.Write(Char.ToUpper(v));
+            }
+            else
+            {
+                Console.Write(Char.ToLower(v));
+            }
+        }
+    }
+}
